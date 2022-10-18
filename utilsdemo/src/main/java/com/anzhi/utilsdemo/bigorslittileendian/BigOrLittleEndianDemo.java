3c753924bbeb34bdac49a160be874e2cc5cfc5fa
azh <2550364626@qq.com>
2022-10-18 12:52:19 +0800

    修改

--- utilsdemo/src/main/java/com/anzhi/utilsdemo/bigorslittileendian/BigOrLittleEndianDemo.java
+++ utilsdemo/src/main/java/com/anzhi/utilsdemo/bigorslittileendian/BigOrLittleEndianDemo.java
@@ -1,18 +1,22 @@
 package com.anzhi.utilsdemo.bigorslittileendian;
 
-import sun.misc.Unsafe;
+/*
+* 需要使用 jdk8 编译，才能找到 sun.misc.Unsafe 这个类
+* jdk11 限制了部分权限使用
+* */
+//import sun.misc.Unsafe;
 
 import java.lang.reflect.Field;
 
 public class BigOrLittleEndianDemo {
-    public static Unsafe reflectGetUnsafe() {
-        try {
-            Field field = Unsafe.class.getDeclaredField("theUnsafe");
-            field.setAccessible(true);
-            return (Unsafe) field.get(null);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+//    public static Unsafe reflectGetUnsafe() {
+//        try {
+//            Field field = Unsafe.class.getDeclaredField("theUnsafe");
+//            field.setAccessible(true);
+//            return (Unsafe) field.get(null);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
 }
