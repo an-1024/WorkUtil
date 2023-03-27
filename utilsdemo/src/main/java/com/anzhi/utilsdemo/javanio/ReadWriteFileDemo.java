bbe9dd2da62cd918a52fcaf2c47797cb8ade0253
azh <2550364626@qq.com>
2023-03-27 14:52:54 +0800

    update

--- utilsdemo/src/main/java/com/anzhi/utilsdemo/javanio/ReadWriteFileDemo.java
+++ utilsdemo/src/main/java/com/anzhi/utilsdemo/javanio/ReadWriteFileDemo.java
@@ -7,7 +7,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStreamReader;
+import java.io.LineNumberReader;
 import java.io.OutputStreamWriter;
+import java.util.stream.Collectors;
 
 /**
  * 读写文件
@@ -15,15 +17,24 @@ import java.io.OutputStreamWriter;
 public class ReadWriteFileDemo {
     public static void main(String[] args) throws Exception{
         String sourcePath = "src/main/resources/sourcepath";
-        String targetPath = "src/main/resources/targetpath/2.txt";
+        String targetPath = "src/main/resources/targetpath/001.txt";
         File file = new File(sourcePath);
         File[] files = file.listFiles();
         for(File sourceFile : files){
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(sourceFile), "utf-8"));
-            BufferedWriter targetWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(targetPath), "utf-8"));
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(sourceFile), "GBK"));
+            BufferedWriter targetWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(targetPath), "GBK"));
+            
+            // 统计文件行数
+            LineNumberReader lineNumberReader = new LineNumberReader(bufferedReader);
+            lineNumberReader.skip(sourceFile.length());
+            int lineNumber = lineNumberReader.getLineNumber();
+
+            System.out.println("----- 文件行数为：" + lineNumber);
             String bufferLine = null;
             while((bufferLine = bufferedReader.readLine()) != null){
                 targetWriter.write(bufferLine);
+                
+                targetWriter.newLine();
                 targetWriter.flush();
             }
         }
