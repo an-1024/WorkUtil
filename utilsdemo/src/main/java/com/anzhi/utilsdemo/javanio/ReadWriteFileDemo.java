b23ea90ffa9178073712738551cbe019f6c592a3
azh <2550364626@qq.com>
2023-03-30 23:14:19 +0800

    文件解析

--- utilsdemo/src/main/java/com/anzhi/utilsdemo/javanio/ReadWriteFileDemo.java
+++ utilsdemo/src/main/java/com/anzhi/utilsdemo/javanio/ReadWriteFileDemo.java
@@ -6,6 +6,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
 import java.io.OutputStreamWriter;
@@ -24,13 +25,13 @@ public class ReadWriteFileDemo {
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(sourceFile), "GBK"));
             BufferedWriter targetWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(targetPath), "GBK"));
 
-            bufferedReader.mark((int)sourceFile.length() + 1);
-            // 统计文件行数
-            LineNumberReader lineNumberReader = new LineNumberReader(bufferedReader);
+            //bufferedReader.mark((int)sourceFile.length() + 1);
+            // 统计文件行数, 创建新的 FileReader 对象统计行数
+            LineNumberReader lineNumberReader = new LineNumberReader(new FileReader(sourceFile));
             lineNumberReader.skip(sourceFile.length());
             int lineNumber = lineNumberReader.getLineNumber();
             // 重置文件指针
-            bufferedReader.reset();
+            //bufferedReader.reset();
 
             System.out.println("----- 文件行数为：" + lineNumber);
             
