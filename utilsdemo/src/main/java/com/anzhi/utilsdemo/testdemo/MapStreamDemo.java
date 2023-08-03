9fcecd572e154da2c39b440d1520b3560ca0c3a6
azh <2550364626@qq.com>
2023-08-04 07:49:38 +0800

    map 判空测试

--- utilsdemo/src/main/java/com/anzhi/utilsdemo/testdemo/MapStreamDemo.java
+++ utilsdemo/src/main/java/com/anzhi/utilsdemo/testdemo/MapStreamDemo.java
@@ -20,21 +20,35 @@ public class MapStreamDemo {
             userDtoList.add(userDto);
         }
 
-        for (int i=0; i<5; i++) {
+        for (int i=5; i<10; i++) {
+            UserDto userDto = new UserDto();
+            userDto.setName("123");
+            userDto.setMoney(String.valueOf(i));
+            userDtoList.add(userDto);
+        }
+
+        for (int i=10; i<11; i++) {
             UserDto userDto = new UserDto();
             userDto.setName("1234");
             userDto.setMoney(String.valueOf(i));
             userDtoList.add(userDto);
         }
 
-        for (int i=0; i<5; i++) {
-            UserDto userDto = new UserDto();
-            userDto.setName("1235");
-            userDto.setMoney(String.valueOf(i));
-            userDtoList.add(userDto);
-        }
-
         Map<String, List<UserDto>> collect = userDtoList.stream().collect(Collectors.groupingBy(UserDto::getName));
         log.info("------------------ {}", JSON.toJSONString(collect));
+        
+        for (int i=0; i<10; i++) {
+            for (int j=0; i<10; i++) {
+                if (j > 0) {
+                    System.out.println("-----------");
+                }
+                break;
+            }
+            System.out.println("--------------1111111");
+        }
+        
+        if (collect.get("12345") == null) {
+            System.out.println("--------------------------333333333");
+        }
     }
 }
