42ba647e6f90f9a319bec89aa57dbbbed5c02750
azh <2550364626@qq.com>
2023-08-06 19:20:28 +0800

    map 判空测试

--- utilsdemo/src/main/java/com/anzhi/utilsdemo/WorkTestApplication.java
+++ utilsdemo/src/main/java/com/anzhi/utilsdemo/WorkTestApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @Package: com.anzhi.worktest
  * @ClassName: WorkTestApplication
@@ -17,6 +21,10 @@ import org.springframework.retry.annotation.EnableRetry;
 @Slf4j
 public class WorkTestApplication {
     public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+        List<String> list1 = new LinkedList<>();
+        
         System.setProperty("workTest", "workTest");
         SpringApplication.run(WorkTestApplication.class, args);
     }
